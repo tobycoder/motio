80cb04a50bc72a4c82816c094af3c5b89bf58b8e
Floris de Boer <floris@florisdeboer.com>
2025-09-21 14:42:13 +0200

    Made compatible for python 3.13

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from app.config import Config
+from .config import Config
 
 # Initialiseer extensions
 db = SQLAlchemy()
@@ -21,7 +21,7 @@ def create_app(config_class=Config):
     #login_manager.login_message = 'Log in om toegang te krijgen tot deze pagina.'
     
     # Importeer models
-    from app.models import User, Party, Motie
+    from .models import User, Party, Motie
     
     # User loader voor Flask-Login
     #@login_manager.user_loader
@@ -29,28 +29,28 @@ def create_app(config_class=Config):
         #return User.query.get(int(user_id))
     
     # Registreer blueprints
-    from app.instrumenten import bp as instrumenten_bp
+    from .instrumenten import bp as instrumenten_bp
     app.register_blueprint(instrumenten_bp, url_prefix='/instrumenten')
     
-    from app.auth import bp as auth_bp
+    from .auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    from app.gebruikers import bp as gebruikers_bp
+    from .gebruikers import bp as gebruikers_bp
     app.register_blueprint(gebruikers_bp, url_prefix='/gebruikers')
 
-    from app.help import bp as help_bp
+    from .help import bp as help_bp
     app.register_blueprint(help_bp, url_prefix='/help')
 
-    from app.partijen import bp as partijen_bp
+    from .partijen import bp as partijen_bp
     app.register_blueprint(partijen_bp, url_prefix='/partijen')
 
-    from app.profiel import bp as profielen_bp
+    from .profiel import bp as profielen_bp
     app.register_blueprint(profielen_bp, url_prefix='/profiel')
 
-    from app.dashboard import bp as dashboard_bp
+    from .dashboard import bp as dashboard_bp
     app.register_blueprint(dashboard_bp)
 
-    from app.front import bp as front_bp
+    from .front import bp as front_bp
     app.register_blueprint(front_bp, url_prefix='/front')
     
     return app
