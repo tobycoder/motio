1f946576d3364f37629155ec9b3d790ba4ca0dcc
Floris de Boer <floris@florisdeboer.com>
2025-09-29 15:30:31 +0200

    Refreshed the database

--- migrations copy/env.py
+++ migrations copy/env.py
@@ -3,11 +3,13 @@ from logging.config import fileConfig
 
 from flask import current_app
 
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", current_app.config["SQLALCHEMY_DATABASE_URI"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
